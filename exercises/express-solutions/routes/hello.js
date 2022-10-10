acb79e0ff4e6259bc99474bd407b221c381952ee
Simon Oulevay <simon.oulevay@gmail.com>
2022-10-10 15:08:44 +0200

    Fix express exercise content type

--- exercises/express-solutions/routes/hello.js
+++ exercises/express-solutions/routes/hello.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', function (req, res, next) {
   const name = req.query.name ?? 'World';
-  res.send(`Hello, ${name}!`);
+  res.set('Content-Type', 'plain/text').send(`Hello, ${name}!`);
 });
 
 export default router;
